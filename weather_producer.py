8b8f4c9578a81c7931f0d0bb3707d50612235c9b
muskan320 <himalyanmuskan@gmail.com>
2026-06-29 21:58:10 +0530

    Add files via upload

--- weather_producer.py
+++ weather_producer.py
@@ -1,3 +1,7 @@
+import json
+import time
+import logging
+
 #import the function that sends weather data to kafka
 #and waits for confirmation that the message was delivered
 from kafka_producer import publish_and_confirmation
@@ -5,4 +9,34 @@ from kafka_producer import publish_and_confirmation
 
 #import the function that fetches weather data
 #from the weather API and returns it as an event
-from weather_service import get_weather_event
+from weather_service import(
+    get_weather_event,
+    publish_and_confirmation,
+    close
+)
+
+def run_weather_producer():
+    while True:
+        try:
+            event=get_weather_event()
+            metadata=publish_and_confirmation(
+                event,
+                key=event["city"]
+            )
+            logging.info(
+                f"""
+published sucessfully
+Topic: {metadata.topic}
+Partition: {metadata.partition}
+Offset: {metadata.offset}
+"""
+     )
+        except Exception as e:
+            logging.exception(
+                f"producer Error:{e}"
+            )
+        finally:
+            time.sleep(30)
+
+
+        

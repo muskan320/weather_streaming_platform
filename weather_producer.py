4b439f0a4694d0901babc423eed5c02a1b394619
muskan320 <himalyanmuskan@gmail.com>
2026-07-03 15:35:38 +0530

    Add files via upload

--- weather_producer.py
+++ weather_producer.py
@@ -1,42 +1,42 @@
-import json
 import time
 import logging
 
-#import the function that sends weather data to kafka
-#and waits for confirmation that the message was delivered
-from kafka_producer import publish_and_confirmation
-
-
-#import the function that fetches weather data
-#from the weather API and returns it as an event
-from weather_service import(
-    get_weather_event,
+# Import functions from kafka_producer
+from .kafka_producer import (
     publish_and_confirmation,
     close
 )
 
+# Import function from weather_service
+from .weather_service import (
+    get_weather_event
+)
+
+
 def run_weather_producer():
     while True:
-        try:
-            event=get_weather_event()
-            metadata=publish_and_confirmation(
-                event,
-                key=event["city"]
-            )
-            logging.info(
-                f"""
-published sucessfully
-Topic: {metadata.topic}
-Partition: {metadata.partition}
-Offset: {metadata.offset}
-"""
-     )
-        except Exception as e:
-            logging.exception(
-                f"producer Error:{e}"
-            )
-        finally:
-            time.sleep(30)
+        print("Fetching weather...")
+
+        event = get_weather_event()
+        print("Event:", event)
+
+        metadata = publish_and_confirmation(
+            event,
+            key=event["city"]
+        )
+
+        print("Sent Successfully!")
+        print(metadata)
+
+        time.sleep(30)
 
 
-        
+if __name__ == "__main__":
+    try:
+        run_weather_producer()
+
+    except KeyboardInterrupt:
+        logging.info("Producer stopped by user.")
+
+    finally:
+        close()

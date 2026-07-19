1f256a3a17339ce1f145f90d841e6c96804a8ad5
muskan320 <himalyanmuskan@gmail.com>
2026-07-19 21:34:42 +0530

    Add files via upload

---
+++ mysql_connection.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+from configs.mysql_config import (
+
+    MYSQL_HOST,
+
+    MYSQL_PORT,
+
+    MYSQL_DATABASE,
+
+    MYSQL_USERNAME,
+
+    MYSQL_PASSWORD
+
+)
+
+connection = mysql.connector.connect(
+
+    host=MYSQL_HOST,
+
+    port=MYSQL_PORT,
+
+    database=MYSQL_DATABASE,
+
+    user=MYSQL_USERNAME,
+
+    password=MYSQL_PASSWORD
+
+)
+
+cursor = connection.cursor()

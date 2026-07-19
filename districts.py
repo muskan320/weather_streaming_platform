53d02a04e40834c726a0989e6f89dfac11fb6e2b
muskan320 <himalyanmuskan@gmail.com>
2026-07-19 23:28:11 +0530

    Add files via upload

---
+++ districts.py
@@ -0,0 +1,80 @@
+"""
+District configuration for Himachal Pradesh.
+Each district has a name, latitude, and longitude.
+"""
+
+HIMACHAL_DISTRICTS = [
+
+    {
+        "city": "Shimla",
+        "latitude": 31.1048,
+        "longitude": 77.1734
+    },
+
+    {
+        "city": "Solan",
+        "latitude": 30.9045,
+        "longitude": 77.0967
+    },
+
+    {
+        "city": "Sirmaur",
+        "latitude": 30.5600,
+        "longitude": 77.3000
+    },
+
+    {
+        "city": "Bilaspur",
+        "latitude": 31.3300,
+        "longitude": 76.7500
+    },
+
+    {
+        "city": "Hamirpur",
+        "latitude": 31.6800,
+        "longitude": 76.5200
+    },
+
+    {
+        "city": "Una",
+        "latitude": 31.4700,
+        "longitude": 76.2700
+    },
+
+    {
+        "city": "Kangra",
+        "latitude": 32.1000,
+        "longitude": 76.2700
+    },
+
+    {
+        "city": "Chamba",
+        "latitude": 32.5500,
+        "longitude": 76.1300
+    },
+
+    {
+        "city": "Kullu",
+        "latitude": 31.9600,
+        "longitude": 77.1100
+    },
+
+    {
+        "city": "Mandi",
+        "latitude": 31.7100,
+        "longitude": 76.9300
+    },
+
+    {
+        "city": "Kinnaur",
+        "latitude": 31.5400,
+        "longitude": 78.2700
+    },
+
+    {
+        "city": "Lahaul-Spiti",
+        "latitude": 32.5700,
+        "longitude": 77.0400
+    }
+
+]
